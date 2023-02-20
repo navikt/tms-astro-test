c08cf36c854dd0e91f97b275db961d7386232068
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-02-20 13:09:35 +0100

    Fjern kommentarer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import react from "@astrojs/react";
-
-// https://astro.build/config
 import node from "@astrojs/node";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: "server",
